2bb7ceadceda9d44a126e7761d9e22a79a82a93d
Marcus Dantas <marcus.dantas@bct.ect.ufrn.br>
2019-03-05 18:18:01 -0300

    Limpando log e git dist

--- dist/src/modules/welcome/util/HandleErros.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const HandleErros_1 = require("app/architecture/erros/HandleErros");
-exports.default = HandleErros_1.HandleErros;
